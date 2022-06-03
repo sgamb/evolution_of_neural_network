4388d1a0511a123c3cbbd97351450817d5ee00ad
Sergey Gambaryan <sgamb2000@gmail.com>
2022-06-03 23:36:51 +0300

    Update the engine init behavior

--- new_game.py
+++ new_game.py
@@ -5,6 +5,9 @@ class Object:
     position: (int, int)
 
     def __init__(self, world):
+        """
+        Set the object position
+        """
         self.position = tuple(np.random.randint(world.shape))
 
 
@@ -18,15 +21,38 @@ class Food(Object):
 
 class World:
     def __init__(self, shape=(8, 8)):
+        """
+        Initialize an empty world of a given shape
+        """
         self.data = np.zeros(shape, dtype=np.int8)
         self.shape = self.data.shape
 
 
 class Engine:
-    def __init__(self, world):
+    def __init__(self, world=World()):
+        """
+        Populate the new world
+        """
         self._world = world
         self._animal = Animal(world)
-        self._food = [Food(world) for _ in range(3)]
+        self._food = [Food(world) for food in range(3)]
+        self._update_world_data()
+        self._print_world_data()
+
+    def _update_world_data(self):
+        """
+        Update the world data
+        """
+        data = self._world.data
+        data[self._animal.position] = 1
+        for food in self._food:
+            data[food.position] = 2
+
+    def _print_world_data(self):
+        """
+        Hello world
+        """
+        print(self._world.data)
 
 
 class Game:
@@ -34,9 +60,7 @@ class Game:
 
 
 def main():
-    world = World()
-    engine = Engine(world)
-    game = Game(engine)
+    engine = Engine()
 
 
 if __name__ == "__main__":
